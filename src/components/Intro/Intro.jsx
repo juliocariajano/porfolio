2192a2d80db7de6819e29185d27ea8301a09a8a9
jcariajano <juliocariajano@gmail.com>
2022-09-13 22:40:38 -0500

    actualizacion

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -29,17 +29,47 @@ const Intro = () => {
           <span style={{ color: darkMode ? "white" : "" }}>Hola! Yo soy</span>
           <span>Julio Cariajano</span>
           <span>
-          Soy Apasionado por la tecnología de profesion Ingeniero en Sistemas Informáticos con habilidades como 
-          Web Full Stack Developer, conocimientos en 
-          Javascript, React, Redux, Node.js, Express, Sequelize, Postgresql, 
-          además de estar en continua especialización, tengo conocimientos y experiencia en 
-          Redes y Telecomunicaciones - Mikrotik Certified MTCCNA - MTCRE - UBIQUITY, Videovigilacia, 
-          Ofimatica, Estadistica Hospitalaria..
-          Soy una persona proactiva, de mente analítica, con grandes habilidades de comunicación y empatía.
-          Mis experiencias me han fortalecido, tanto en la tolerancia, cómo en el autocontrol ante las 
-          adversidades, lo que favorece mi adaptación al cambio.
+          
+          Full Stack Developer.
           
           </span>
+          <span>
+          Hola soy Julio, Ingeniero en Sistemas Informaticos, soy apasionado por la tecnologia, me encuentro en 
+          continua actualizacion y especializacion de las tecnologias que ya manejo y a su vez investigando nuevas.
+          Tengo conocimiento y experiencia en Redes y Telecomunicaciones Mikrotik Certified MTCNA - MTCRE - UBIQUITY, 
+          Ofimatica, Estadistica Hospitalaria.
+          </span>
+          <span>
+              
+          Tengo habilidades como Web Full Stack Developer:
+          </span>
+          <span>
+                    
+          Backend: conocimientos en Node js, Express   
+          
+          </span>
+          <span>
+          
+                    Frontend: conocimientos en Html, CSS, Javascript, React, Redux, Boostrap, Material UI, Framer motion.
+                    
+                    </span>
+                    <span>
+          
+          Base de Datos: conocimientos en Sequelize, Postgresql, Mysql
+
+          </span>
+          <span>
+
+          Soy una persona proactiva, de mente analitica, me desempeño bien trabajando en equipo ya que poseo buena 
+          comunicacion y empatia.
+
+          Mis experiencias me han fortalecido en la tolerancia y el autocontrol a la hora de enfrentar diferentes tipos 
+          de adversidades que puedan surgir en el que hacer laboral, otorgandome flexibilidad para poder adaptarme a 
+          distintas situaciones de cambio
+
+          </span>
+
+
         </div>
         <Link to="contact" smooth={true} spy={true}>
           <button className="button i-button">Hire me</button>
